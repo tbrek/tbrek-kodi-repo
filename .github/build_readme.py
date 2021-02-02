1cd0722623d47f4b5d1cae046fd979b4060ce7f1
tbrek <me@tombrek.com>
2021-02-02 01:46:04 +0000

    Squash me

--- .github/build_readme.py
+++ .github/build_readme.py
@@ -61,29 +61,30 @@ def main():
                 args.repo_name,
             )
             for addon in tree.getroot():
-                if addon.tag == 'addon':
-                    addon_id = addon.get('id')
-                    addon_nm = addon.get('name')
-                    addon_ver = addon.get('version')
-                    plugin_zip_link = '{branch}/{datadir}/{id}/{id}-{ver}.zip'.format(
-                        branch=b['name'], id=addon_id, ver=addon_ver, datadir=args.datadir)
-                    addons.append(
-                        Addon(addon_id, addon_nm, addon_ver, plugin_zip_link)
-                    )
-                    if repo_addon_id == addon_id and not repo_addon_link:
-                        repo_addon_link = plugin_zip_link
-    
-                    addons_text = addons_text + '\n\nKodi {maxversion} ([__{branchname}__]({branchlink}/addons.xml)):' \
-                        '\n\n{branchaddons}'.format(
-                            branchname=b['name'],
-                            branchlink=b['name'],
-                            maxversion=b['maxversion'][:2],
-                            branchaddons='\n'.join([
-                                    '- [__{nm}__]({link}) {id} v{ver}'.format(
-                                        id=a.id, nm=a.name, link=a.zip, ver=a.version)
-                                    for a in addons
-                                ])
-                        )
+                if not addon.tag == 'addon':
+                    continue
+                addon_id = addon.get('id')
+                addon_nm = addon.get('name')
+                addon_ver = addon.get('version')
+                plugin_zip_link = '{branch}/{datadir}/{id}/{id}-{ver}.zip'.format(
+                    branch=b['name'], id=addon_id, ver=addon_ver, datadir=args.datadir)
+                addons.append(
+                    Addon(addon_id, addon_nm, addon_ver, plugin_zip_link)
+                )
+                if repo_addon_id == addon_id and not repo_addon_link:
+                    repo_addon_link = plugin_zip_link
+
+            addons_text = addons_text + '\n\nKodi {maxversion} ([__{branchname}__]({branchlink}/addons.xml)):' \
+                '\n\n{branchaddons}'.format(
+                    branchname=b['name'],
+                    branchlink=b['name'],
+                    maxversion=b['maxversion'][:2],
+                    branchaddons='\n'.join([
+                            '- [__{nm}__]({link}) {id} v{ver}'.format(
+                                id=a.id, nm=a.name, link=a.zip, ver=a.version)
+                            for a in addons
+                        ])
+                )
 
         template_string = template_file.read()
         output_file.write(template_string.format(
